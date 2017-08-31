387fa67378c83ee42338823382de91f3e70a200d
jdcadenas <jdcadenas@gmail.com>
2017-08-31 18:15:55 -0400

    agregando sonido

--- app/src/main/java/com/example/jdcadenas/mislugares/MainActivity.java
+++ app/src/main/java/com/example/jdcadenas/mislugares/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jdcadenas.mislugares;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,22 +17,31 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button bacercaDe;
     private Button bSalir;
     private Button bPreferencias;
     public static Lugares lugares = new LugaresVector();
-
+    public MediaPlayer mp;
     private RecyclerView recyclerView;
     public AdaptadorLugares adaptador;
     private RecyclerView.LayoutManager layoutManager;
 
     @Override
+    protected void onPause() {
+        Toast.makeText(this, "en pausa", Toast.LENGTH_SHORT).show();
+        mp.pause();
+        super.onPause();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mp = MediaPlayer.create(this, R.raw.audio);
+        mp.start();
       //  bacercaDe = (Button) findViewById(R.id.button03);
       //  bSalir = (Button) findViewById(R.id.button04);
        // bPreferencias = (Button) findViewById(R.id.button02);
--- app/src/main/java/com/example/jdcadenas/mislugares/VistaLugarActivity.java
+++ app/src/main/java/com/example/jdcadenas/mislugares/VistaLugarActivity.java
@@ -1,9 +1,15 @@
 package com.example.jdcadenas.mislugares;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -12,7 +18,10 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -24,15 +33,19 @@ public class VistaLugarActivity extends AppCompatActivity {
     private long id;
     private Lugar lugar;
     final static int RESULTADO_EDITAR = 1;
-
+    private ImageView imageView;
+    final static int RESULTADO_GALERIA = 2;
+    final static int RESULTADO_FOTO = 3;
+    private Uri uriFoto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vista_lugar);
         Bundle extras = getIntent().getExtras();
         id = extras.getLong("id",0);
-
+        imageView = (ImageView) findViewById(R.id.foto);
        actualizarVistas();
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
 
     }
    @Override
@@ -68,6 +81,12 @@ public class VistaLugarActivity extends AppCompatActivity {
          return super.onOptionsItemSelected(item);
       }
    }
+
+    public void galeria(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, RESULTADO_GALERIA);
+    }
     public void verMapa(View view) {
         Uri uri;
         double lat = lugar.getPosicion().getLatitud();
@@ -103,9 +122,45 @@ public class VistaLugarActivity extends AppCompatActivity {
         {
             actualizarVistas();
             findViewById(R.id.scrollView1).invalidate();
+        } else if (requestCode == RESULTADO_GALERIA) {
+            if (resultCode == Activity.RESULT_OK) {
+                lugar.setFoto(data.getDataString());
+                ponerFoto(imageView, lugar.getFoto());
+            } else {
+                Toast.makeText(this, "Foto no cargada", Toast.LENGTH_LONG).show();
+            }
+        } else if (requestCode == RESULTADO_FOTO) {
+            if (resultCode == Activity.RESULT_OK
+                    && lugar != null && uriFoto != null) {
+                lugar.setFoto(uriFoto.toString());
+                ponerFoto(imageView, lugar.getFoto());
+            } else {
+                Toast.makeText(this, "Error en captura", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
+    protected void ponerFoto(ImageView imageView, String uri) {
+        if (uri != null) {
+            imageView.setImageBitmap(reduceBitmap(this, uri, 1024, 1024));
+        } else {
+            imageView.setImageBitmap(null);
+        }
+    }
+
+    public void tomarFoto(View view) {
+        Intent intent = new Intent("MediaStore.ACTION_IMAGE_CAPTURE");
+        uriFoto = Uri.fromFile(new File(
+                Environment.getExternalStorageDirectory() + File.separator
+                        + "img_" + (System.currentTimeMillis() / 1000) + ".jpg"));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uriFoto);
+        startActivityForResult(intent, RESULTADO_FOTO);
+    }
+
+    public void eliminarFoto(View view) {
+        lugar.setFoto(null);
+        ponerFoto(imageView, null);
+    }
     public void borrarLugar(final int id) {
        new AlertDialog.Builder(this)
                .setTitle("Borrado de lugar")
@@ -128,6 +183,28 @@ public class VistaLugarActivity extends AppCompatActivity {
 
 
     }
+
+    public static Bitmap reduceBitmap(Context contexto, String uri,
+                                      int maxAncho, int maxAlto) {
+        try {
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(contexto.getContentResolver()
+                    .openInputStream(Uri.parse(uri)), null, options);
+            options.inSampleSize = (int) Math.max(
+                    Math.ceil(options.outWidth / maxAncho),
+                    Math.ceil(options.outHeight / maxAlto));
+            options.inJustDecodeBounds = false;
+            return BitmapFactory.decodeStream(contexto.getContentResolver()
+                    .openInputStream(Uri.parse(uri)), null, options);
+        } catch (FileNotFoundException e) {
+            Toast.makeText(contexto, "Fichero/recurso no encontrado",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 public void actualizarVistas(){
      lugar = MainActivity.lugares.elemento((int) id);
         TextView nombre = (TextView) findViewById(R.id.nombre);
@@ -160,7 +237,7 @@ public void actualizarVistas(){
                     lugar.setValoracion(valor);
                 }
         });
-
+    ponerFoto(imageView, lugar.getFoto());
 }
 
 
